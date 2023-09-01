8aaf31dea317294cceb4e396133044030883e0e6
Alex <tindall.al@gmail.com>
2023-08-31 21:23:30 -0500

    Add project files.

---
+++ FileManager.cs
@@ -0,0 +1,39 @@
+﻿namespace Class02File;
+
+public class FileManager
+{
+    public void fileWrite(string fileName)
+    {
+        StreamWriter sw = new StreamWriter(fileName, true);
+
+        Console.WriteLine("Enter a course");
+        var course = Console.ReadLine();
+
+        Console.WriteLine("Enter a grade");
+        var grade = Console.ReadLine();
+
+        sw.WriteLine($"{course}|{grade}");
+
+
+        sw.Close();
+    }
+
+    public void fileRead(string fileName)
+    {
+        StreamReader sr = new StreamReader(fileName);
+
+        // reads the first line to bypass the header
+        sr.ReadLine();
+        while (sr.EndOfStream != true)
+        {
+            var line = sr.ReadLine();
+            Console.WriteLine($"Your line is {line}");
+
+            // parse the line to retrieve the grade
+            // .Split() takes a char value not a string
+            var lineArray = line.Split('|');
+
+            Console.WriteLine($"Course: {lineArray[0]}. Grade: {lineArray[1]}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Design;
+
+namespace Class02File
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //*********************************** How To Create A File Using c# **************************************
+
+            FileManager fileManager = new FileManager();
+            string menu;
+            string fileName = "grades.txt";
+            
+
+            do
+            {
+                // Menu Structure for assignment
+                Console.WriteLine("1. Write data to file");
+                Console.WriteLine("2. Read data from file");
+                Console.WriteLine("3. Exit");
+                menu = Console.ReadLine();
+
+                if (menu == "1")
+                {
+                    fileManager.fileWrite(fileName);
+                }
+                else if (menu == "2")
+                {
+                    if (File.Exists(fileName))
+                    {
+                        fileManager.fileRead(fileName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+
+                }
+                 
+            } while (menu != "3");
+        }
+    }
+}
